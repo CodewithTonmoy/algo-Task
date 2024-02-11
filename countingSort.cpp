ca1132551b104afa6eec66433baa82e28d74bdbe
TonmoyBonik <101162384+CodewithTonmoy@users.noreply.github.com>
2024-02-11 14:21:12 +0600

    Create countingSort.cpp

---
+++ countingSort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+void countingSort(std::vector<int>& arr) {
+    
+    int maxElement = *std::max_element(arr.begin(), arr.end());
+
+    
+    std::vector<int> count(maxElement + 1, 0);
+
+    
+    for (int num : arr) {
+        count[num]++;
+    }
+
+   
+    for (int i = 1; i <= maxElement; ++i) {
+        count[i] += count[i - 1];
+    }
+
+    
+    std::vector<int> result(arr.size());
+    
+   
+    for (int i = arr.size() - 1; i >= 0; --i) {
+        result[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+   
+    for (size_t i = 0; i < arr.size(); ++i) {
+        arr[i] = result[i];
+    }
+}
+
+int main() {
+    
+    std::vector<int> arr = {4, 2, 7, 1, 3, 9, 5, 8, 6};
+    
+    std::cout << "Original array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+
+    countingSort(arr);
+
+    std::cout << "\nSorted array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
