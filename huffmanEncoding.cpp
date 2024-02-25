7e2c52920aa680d348f28dd6b976e358a7022a12
TonmoyBonik <101162384+CodewithTonmoy@users.noreply.github.com>
2024-02-25 14:43:48 +0600

    Create huffmanEncoding.cpp

---
+++ huffmanEncoding.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <string>
+using namespace std;
+
+// A Huffman tree node
+struct MinHeapNode {
+    char data;
+    unsigned frequency;
+    MinHeapNode *left, *right;
+
+    MinHeapNode(char data, unsigned frequency) {
+        this->data = data;
+        this->frequency = frequency;
+        left = right = nullptr;
+    }
+};
+
+// Comparison function for priority queue
+struct compare {
+    bool operator()(MinHeapNode* l, MinHeapNode* r) {
+        return (l->frequency > r->frequency);
+    }
+};
+
+// Traverse Huffman tree and store codes in a map
+void encodeHuffman(MinHeapNode* root, string str, map<char, string>& codes) {
+    if (!root)
+        return;
+
+    // If the node is a leaf node
+    if (!root->left && !root->right) {
+        codes[root->data] = str;
+    }
+
+    // Traverse left
+    encodeHuffman(root->left, str + "0", codes);
+    // Traverse right
+    encodeHuffman(root->right, str + "1", codes);
+}
+
+// Build Huffman tree and encode
+map<char, string> buildHuffmanTree(string text) {
+    map<char, unsigned> freq;
+    // Calculate frequency of each character
+    for (char ch : text) {
+        freq[ch]++;
+    }
+
+    // Create a priority queue to store nodes
+    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> pq;
+
+    // Create a node for each character and add it to the priority queue
+    for (auto pair : freq) {
+        pq.push(new MinHeapNode(pair.first, pair.second));
+    }
+
+    // Build Huffman tree
+    while (pq.size() != 1) {
+        MinHeapNode *left = pq.top();
+        pq.pop();
+
+        MinHeapNode *right = pq.top();
+        pq.pop();
+
+        unsigned sum = left->frequency + right->frequency;
+        pq.push(new MinHeapNode('$', sum));
+    }
+
+    MinHeapNode* root = pq.top();
+
+    // Traverse the Huffman tree and store codes
+    map<char, string> codes;
+    encodeHuffman(root, "", codes);
+
+    return codes;
+}
+
+int main() {
+    string text = "hello world";
+    
+    // Build Huffman tree and get codes
+    map<char, string> codes = buildHuffmanTree(text);
+
+    // Print the Huffman codes
+    cout << "Huffman Codes:\n";
+    for (auto pair : codes) {
+        cout << pair.first << " : " << pair.second << endl;
+    }
+
+    return 0;
+}
